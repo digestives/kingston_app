5d26615293e7afcea9817dbf9b8c3aa6151ee2d9
Patrick Magee <patrick.magee@live.co.uk>
2011-05-10 22:47:08 +0100

    Posts can now be deleted by admins

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,13 @@ class PostsController < ApplicationController
   	end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    flash[:info] = "Post deleted."
+    redirect_to posts_path
+  end
+
   private
 
     def authenticate
