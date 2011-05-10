41e0aa15aa9b5084dbcb6392dc1ece77000e8d05
Patrick Magee <patrick.magee@live.co.uk>
2011-05-10 18:32:30 +0100

    Added missing migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110509200919) do
+ActiveRecord::Schema.define(:version => 20110510172824) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(:version => 20110509200919) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",              :default => false
-    t.integer  "membership_id"
   end
 
 end
+
