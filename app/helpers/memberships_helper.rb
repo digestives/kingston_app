54704717347eca63be6cc0092961883c8ec87a35
Patrick Magee <patrick.magee@live.co.uk>
2011-05-10 19:50:12 +0100

    comments in membership helper

--- app/helpers/memberships_helper.rb
+++ app/helpers/memberships_helper.rb
@@ -2,8 +2,9 @@ module MembershipsHelper
 
   def display_privilege(privilege, membership)
 
+    # checks if its true or false
     if privilege
-
+      # if its true, or exists, we check if its a value
       if privilege == membership.guests && privilege > 0
         privilege
       elsif privilege == membership.guests && privilege < 1
@@ -11,7 +12,6 @@ module MembershipsHelper
       else
         image_tag("/stylesheets/blueprint/plugins/buttons/icons/tick.png")
       end
-
     else
       image_tag("/stylesheets/blueprint/plugins/buttons/icons/cross.png")
     end
