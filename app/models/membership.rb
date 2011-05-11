e92e8d772919e23b5234a7e65585f7c081cf606a
Patrick Magee <patrick.magee@live.co.uk>
2011-05-11 18:29:41 +0100

    Added validation so that pool must be selected if tennis or sauna priviledges are not

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -21,11 +21,15 @@ class Membership < ActiveRecord::Base
 
 	NUM_GUESTS_ALLOWED = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
+  attr_accessible :pool, :sauna, :tennis, :name, :description, :fee, :guests
+
 	has_many :users, :through => :subscriptions
 
 	validates :name, :presence => true, :length => { :within => 3..150 }
 
 	validates :description, :presence => true
+
+  validates :pool, :inclusion => { :in => [true], :message => "must be granted if tennis or sauna is not" }, :if => lambda { |m| !m.tennis && !m.sauna }
 
 	validates :fee, :presence => true
 
