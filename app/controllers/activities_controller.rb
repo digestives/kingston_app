66aa812c2fde1c8b8de438d5526fe7ae6f1d6d77
Patrick Magee <patrick.magee@live.co.uk>
2011-05-13 22:16:00 +0100

    Beginning of admin users being able to delete an activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
   def index
 		@activities = Activity.all
 	end
-	
+
 	def show
     @activity = Activity.find(params[:id])
     @title = @activity.id
@@ -12,7 +12,7 @@ class ActivitiesController < ApplicationController
   def new
   	@activity = Activity.new
   end
-  
+
   def create
   	@activity = Activity.new(params[:activity])
     if @activity.save
@@ -21,7 +21,21 @@ class ActivitiesController < ApplicationController
   	else
       @title = "New Activity"
       render 'new'
-    end	
+    end
+  end
+
+  def destroy
+    @activity = Activity.find(params[:id])
+
+    if @activity.users.empty?
+      flash[:notice] = "no users have booked this activity"
+    else
+      flash[:notice] = "You can not delete an activity which has been booked"
+    end
+
+    redirect_to activities_path
+
   end
 
 end
+
